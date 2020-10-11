822c5f6f3afba40ea36fdc3de59b19ff29501923
Supreetha <sbangaloreso@umass.edu>
2020-10-11 19:06:18 -0400

    Added mapper function and master functions

--- src/main/java/org/systemsfords/p1/mr/App.java
+++ src/main/java/org/systemsfords/p1/mr/App.java
@@ -1,13 +1,42 @@
 package org.systemsfords.p1.mr;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+    	ProcessBuilder processBuilder = new ProcessBuilder();
+
+        processBuilder.command("java", "-cp", "/Users/supreethabs/eclipse-workspace/MapReduceImplementation/target/mr-0.0.1-SNAPSHOT.jar", "org.systemsfords.p1.mr.Mapper");
+
+        try {
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/systemsfords/p1/mr/Mapper.java
+++ src/main/java/org/systemsfords/p1/mr/Mapper.java
@@ -1,12 +1,45 @@
 package org.systemsfords.p1.mr;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Mapper {
 
 	public static void main(String args[]) {
-		map();
+		String fileName = "public/sample.txt";
+		String contentsFile = readFile(fileName);
+		map("public/sample.txt", contentsFile);
 	}
 	
-	public static void map() {
-		System.out.println("In mapper function");
+	public static void map(String key, String value) {
+		String processedContents = value.replaceAll("\\p{Punct}", " ");
+		String[] terms = processedContents.split("\\s+");
+		try {
+            PrintWriter writer = new PrintWriter(new FileWriter("public/tokenized.txt", false));
+            for (String term : terms) {
+    			writer.println(term + ", " + 1);
+    		}
+            writer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 	}
+	
+	public static String readFile(String fileName) {
+        StringBuilder sb  = new StringBuilder();
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+            String s;
+            while((s = br.readLine()) != null) {
+                sb.append(s);
+            }
+            fr.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return sb.toString();
+    }
 }

704e0995041f50447f1975d40e3da1ac3c581bf9
Supreetha <sbangaloreso@umass.edu>
2020-10-11 18:10:49 -0400

    Added a mapper class

---
+++ src/main/java/org/systemsfords/p1/mr/Mapper.java
@@ -0,0 +1,12 @@
+package org.systemsfords.p1.mr;
+
+public class Mapper {
+
+	public static void main(String args[]) {
+		map();
+	}
+	
+	public static void map() {
+		System.out.println("In mapper function");
+	}
+}

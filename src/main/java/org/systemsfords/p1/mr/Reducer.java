87d15283a6e3f3be08eedf54a04c88da60503800
Supreetha <sbangaloreso@umass.edu>
2020-10-11 19:01:43 -0400

    Added Reducer and a mapper function

---
+++ src/main/java/org/systemsfords/p1/mr/Reducer.java
@@ -0,0 +1,12 @@
+package org.systemsfords.p1.mr;
+
+public class Reducer {
+	
+	public static void main(String args[]) {
+		reduce();
+	}
+	
+	public static void reduce() {
+		System.out.println("In reduce function");
+	}
+}

a7d5498a65ed2246e0a883e1359e684edb8f3ff5
Supreetha <sbangaloreso@umass.edu>
2020-11-06 20:56:13 -0500

    Replaced ant with mvn

--- src/main/resources/runScript.sh
+++ src/main/resources/runScript.sh
@@ -1,4 +1,4 @@
-ant
+mvn clean package
 java -cp target/mr-0.0.1-SNAPSHOT.jar org.systemsfords.p1.mr.MasterLibrary configFileInvertedIndex.txt
 python3 testInvertedIndex.py
 java -cp target/mr-0.0.1-SNAPSHOT.jar org.systemsfords.p1.mr.MasterLibrary configFile.txt
